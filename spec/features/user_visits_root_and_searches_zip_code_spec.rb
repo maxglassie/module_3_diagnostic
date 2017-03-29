c2a637319a5d589d94cec78b352664bc255c56ad
Max Glassie <msglassie@gmail.com>
2017-03-29 09:19:01 -0600

    adds user story and creates spec files for the nrel API also, hit the API using postman and my secret key

---
+++ spec/features/user_visits_root_and_searches_zip_code_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.feature "user visits root page", :vcr do
+  scenario "user searches for zipcode", :vcr do
+      # As a user
+      # When I visit "/"
+      # And I fill in the search form with 80203
+      # And I click "Locate"
+      # Then I should be on page "/search" with parameters visible in the url
+      # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
+      # And the stations should be limited to Electric and Propane
+      # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
+  end
+end

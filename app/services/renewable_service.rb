73452eb6143c77ddb8f7b38805417982fe7c05bc
Max Glassie <msglassie@gmail.com>
2017-03-29 10:11:39 -0600

    completes renewable service with correct API response for location, next building PORO to pass to views

--- app/services/renewable_service.rb
+++ app/services/renewable_service.rb
@@ -2,10 +2,12 @@ class RenewableService
 
   def initialize(zipcode)
     @zipcode = zipcode
+    @api_key = ENV['api_key']
   end
 
   def stations
+    @stations_response = Faraday.get("http://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{@api_key}&location=#{@zipcode}&fuel_type=ELEC&limit=1")
     binding.pry
-    @stations = Faraday.get(http://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=4i5DPYV18eY4Tjke3vHDV5x61WN6hmpgeWZBrup1&location=80203&fuel_type=ELEC&limit=1)
+    @stations = JSON.parse(@stations_response.body)
   end
 end
--- spec/services/renewable_service_spec.rb
+++ spec/services/renewable_service_spec.rb
@@ -1,14 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe "query service hits API" do
+RSpec.describe "query service hits API", :vcr do
   it "hits the station API with zipcode and electric and returns JSON" do
-    #creates a new renewable_service
-    #and passes zipcode as params
-    #it returns an open struct object
-    #and when queried that open struct object returns
-    #stations.elec["total"]
-    #returns 120
-    
-    response = JSON.parse(RenewableService.new(80203))
+    response = RenewableService.new("80203").stations
   end
+  
 end
